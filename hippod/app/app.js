dcfec0f0b79837e3949d458a468a520fdb85e2e9
Hagen Paul Pfeifer <hagen@jauu.net>
2015-11-02 23:51:13 +0100

    split page in several pages via ui-router
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- hippod/app/app.js
+++ hippod/app/app.js
@@ -1,107 +1,23 @@
-var mymodal = angular.module('helloApp', ['ui.bootstrap']);
+var hippoD = angular.module('helloApp', ['ui.router', 'ui.bootstrap']);
 
+hippoD.config(function($stateProvider, $urlRouterProvider) {
 
+	$urlRouterProvider.otherwise('/');
 
-mymodal.controller('MainCtrl', function ($scope) {
-    $scope.showModal = false;
-    $scope.toggleModal = function(){
-        $scope.showModal = !$scope.showModal;
-        //$dialog.dialog({}).open('modalContent.html');  
-    };
-  });
+	$stateProvider
+        .state('dashboard', {
+            url: '/',
+            templateUrl: 'templates/home.html'
+        })
 
-mymodal.directive('modal', function () {
-    return {
-      template: '<div class="modal fade">' + 
-          '<div class="modal-dialog">' + 
-            '<div class="modal-content">' + 
-              '<div class="modal-header">' + 
-                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
-                '<h4 class="modal-title">{{ title }}</h4>' + 
-              '</div>' + 
-              '<div class="modal-body" ng-transclude></div>' + 
-            '</div>' + 
-          '</div>' + 
-        '</div>',
-      restrict: 'E',
-      transclude: true,
-      replace:true,
-      scope:true,
-      link: function postLink(scope, element, attrs) {
-        scope.title = attrs.title;
+        .state('items', {
+            url: '/items',
+            templateUrl: 'templates/items.html'
+        })
 
-        scope.$watch(attrs.visible, function(value){
-          if(value == true)
-            $(element).modal('show');
-          else
-            $(element).modal('hide');
+        .state('admin', {
+            url: '/admin',
+            templateUrl: 'templates/admin.html'
         });
-
-        $(element).on('shown.bs.modal', function(){
-          scope.$apply(function(){
-            scope.$parent[attrs.visible] = true;
-          });
-        });
-
-        $(element).on('hidden.bs.modal', function(){
-          scope.$apply(function(){
-            scope.$parent[attrs.visible] = false;
-          });
-        });
-      }
-    };
-  });
-
-
-
-mymodal.controller('ModalDemoCtrl', function ($scope, $uibModal, $log) {
-
-  $scope.items = ['item1', 'item2', 'item3'];
-
-  $scope.animationsEnabled = true;
-
-  $scope.open = function (size) {
-
-    var modalInstance = $uibModal.open({
-      animation: $scope.animationsEnabled,
-      templateUrl: 'modal-object-item.html',
-      controller: 'ModalInstanceCtrl',
-      size: size,
-      resolve: {
-        items: function () {
-          return $scope.items;
-        }
-      }
-    });
-
-    modalInstance.result.then(function (selectedItem) {
-      $scope.selected = selectedItem;
-    }, function () {
-      $log.info('Modal dismissed at: ' + new Date());
-    });
-  };
-
-  $scope.toggleAnimation = function () {
-    $scope.animationsEnabled = !$scope.animationsEnabled;
-  };
-
-});
-
-// Please note that $modalInstance represents a modal window (instance) dependency.
-// It is not the same as the $uibModal service used above.
-
-mymodal.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
-
-  $scope.items = items;
-  $scope.selected = {
-    item: $scope.items[0]
-  };
-
-  $scope.ok = function () {
-    $uibModalInstance.close($scope.selected.item);
-  };
-
-  $scope.cancel = function () {
-    $uibModalInstance.dismiss('cancel');
-  };
+        
 });
