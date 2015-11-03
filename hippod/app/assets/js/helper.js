85459b18b7a6f8b3842e0ef495a598fc8ec6faf7
Hagen Paul Pfeifer <hagen@jauu.net>
2015-11-03 23:21:01 +0100

    display usage in human readable form
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

---
+++ hippod/app/assets/js/helper.js
@@ -0,0 +1,31 @@
+
+
+function prettyNumber(pBytes, pUnits) {
+    if(pBytes == 0) return '0 Bytes';
+    if(pBytes == 1) return '1 Byte';
+    if(pBytes == -1) return '-1 Byte';
+
+    var bytes = Math.abs(pBytes)
+    if(pUnits && pUnits.toLowerCase() && pUnits.toLowerCase() == 'si') {
+        // SI units use the Metric representation based on 10^3 as a order of magnitude
+        var orderOfMagnitude = Math.pow(10, 3);
+        var abbreviations = ['Bytes', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    } else {
+        // IEC units use 2^10 as an order of magnitude
+        var orderOfMagnitude = Math.pow(2, 10);
+        var abbreviations = ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+    }
+    var i = Math.floor(Math.log(bytes) / Math.log(orderOfMagnitude));
+    var result = (bytes / Math.pow(orderOfMagnitude, i));
+
+    // This will get the sign right
+    if(pBytes < 0) {
+        result *= -1;
+    }
+
+    if(result >= 99.995 || i==0) {
+        return result.toFixed(0) + ' ' + abbreviations[i];
+    } else {
+        return result.toFixed(2) + ' ' + abbreviations[i];
+    }
+}
--- hippod/app/controllers/admin.js
+++ hippod/app/controllers/admin.js
@@ -3,9 +3,11 @@
 hippoD.controller("AdminCtrl", function ($scope, ResourcesService) {
 
 	ResourcesService.getRessource().then(function(res) {
-		$scope.overall_data_byte = res['data']['data']['overall'];
+		$scope.overall_data_byte_human = prettyNumber(res['data']['data']['overall'], 'iec');
+		$scope.overall_data_byte_vanilla = res['data']['data']['overall'];
 	}, function(error) {
-		$scope.overall_data_byte = "unknown (API error)";
+		$scope.overall_data_byte_human = "unknown (API error)";
+		$scope.overall_data_byte_vanilla = "unknown (API error)";
 	});
 
 });
