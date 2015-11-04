7a4899b96ce4d46ea157e5505a9c0eda4cdb0c21
Hagen Paul Pfeifer <hagen@jauu.net>
2015-11-04 23:25:57 +0100

    use modal dialog
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- hippod/app/controllers/items.js
+++ hippod/app/controllers/items.js
@@ -1,13 +1,59 @@
 "use strict";
 
-hippoD.controller("ItemsCtrl", function ($scope, ItemService) {
+hippoD.controller("ItemsCtrl", function ($scope, ItemService, $uibModal, $log) {
 
+	// if called we fill the data
 	ItemService.getItemList().then(function(res) {
-                $scope.data = res.data;
+    $scope.data = res.data;
 		console.log(res.data);
 	}, function(error) {
 		console.log(res);
 	});
 
+
+  $scope.items = ['item1', 'item2', 'item3'];
+
+  $scope.open = function (sha_id) {
+
+    var modalInstance = $uibModal.open({
+      animation: false,
+      templateUrl: 'templates/modal-object-item.html',
+      controller: 'ModalInstanceCtrl',
+      size: 'lg',
+      resolve: {
+        items: function () {
+          return $scope.items;
+        },
+        id: function () {
+          return sha_id;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
 });
 
+
+hippoD.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items, id) {
+
+	$scope.id = id;
+  $scope.items = items;
+
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $uibModalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+});
