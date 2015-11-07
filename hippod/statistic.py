03fc653336207039e38512c8d2a181dd30432c7c
Hagen Paul Pfeifer <hagen@jauu.net>
2015-11-07 15:35:57 +0100

    do not account every written byte, just larger (1000 byte) chunks
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- hippod/statistic.py
+++ hippod/statistic.py
@@ -19,7 +19,7 @@ def folder_size(folder):
     return total_size
 
 
-def stats_are_uptodate(path, today):
+def stats_written_today(path, today):
     with open(path) as data_file:
         data = json.load(data_file)
         if not len(data['item-bytes-overtime']) > 0:
@@ -34,13 +34,22 @@ def update_global_db_stats():
     stat_path = app.config['DB_STATISTICS_FILEPATH']
     today = datetime.datetime.now().strftime('%Y-%m-%d')
     
-    uptodate, data = stats_are_uptodate(stat_path, today)
+    uptodate, data = stats_written_today(stat_path, today)
     if uptodate:
         return
 
     db_path = app.config['DB_OBJECT_PATH']
-    comulative_size = folder_size(db_path)
-    data['item-bytes-overtime'].append([today, comulative_size, 0])
+    new_size = folder_size(db_path)
+
+    # XXX: this assumes that the DB *only* can grow
+    if new_size <= data['item-bytes-overtime'][-1][1] + 1000:
+        # the size do not differ greatly (1K) from the last one
+        # we do *not* write tiny changes here to keep bookkepping
+        # information smaller. Sammler writtes are usually from new
+        # tests results, but no new data objects.
+        return
+
+    data['item-bytes-overtime'].append([today, new_size, 0])
     d_jsonfied =  json.dumps(data, sort_keys=True,indent=4, separators=(',', ': '))
     with open(stat_path, "w+") as f:
         f.write(d_jsonfied)
