648acc0fff28667a88fe18f71b23d6966369b0ed
Hagen Paul Pfeifer <hagen@jauu.net>
2015-11-14 17:18:34 +0100

    preparse code for conditional rendering (e.g markdown)
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- hippod/api_data_get.py
+++ hippod/api_data_get.py
@@ -16,29 +16,25 @@ import hippod.api_err_obj
 import hippod.mime_data_db
 
 
-def get_data(sha_sum):
+
+def object_data_get_int(sha_sum, req_data):
     if not hippod.mime_data_db.obj_available(sha_sum):
-        msg = "File not available"
+        msg = "Data ({}) not available".format(sha_sum)
         raise hippod.api_err_obj.ApiError(msg, 400)
     attr_obj = hippod.mime_data_db.get_attr_obj(sha_sum)
     decompressed = hippod.mime_data_db.is_attr_compressed(attr_obj)
     data = hippod.mime_data_db.get_data(sha_sum, decompress=decompressed, encode_base64=False)
     return attr_obj['mime-type'], data
-
-
-def object_data_get_int(sha_sum):
-    return get_data(sha_sum)
     
 
 @hippod.app.route('/api/v1/data/<sha_id>', methods=['GET', 'POST'])
 def object_data_get(sha_id):
     try:
         start = time.clock()
-        mime_type, data = object_data_get_int(sha_id)
+        req_obj = flask.request.get_json(force=False)
+        mime_type, data = object_data_get_int(sha_id, req_obj)
         end = time.clock()
     except hippod.api_err_obj.ApiError as e:
         return e.transform()
-    #except Exception as e:
-    #    return ApiError(str(e), 500).transform()
 
     return flask.Response(data, mimetype=mime_type)
