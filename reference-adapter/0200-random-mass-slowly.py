4c62d247269cc27a506ad9ec2f4f9a7a443ce58c
Hagen Paul Pfeifer <hagen@jauu.net>
2015-11-20 21:51:10 +0100

    reference image in markdown
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- reference-adapter/0200-random-mass-slowly.py
+++ reference-adapter/0200-random-mass-slowly.py
@@ -122,6 +122,8 @@ after maintainable products.
 
 ![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")
 
+![image.png](api/v1/data/1102e55d428abf3cafadf83e58ea39cee02f53ae)
+
 
 
 
