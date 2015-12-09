c95ee66f73a6ae9f139e84aab7a5f16453d9a848
Hagen Paul Pfeifer <hagen@jauu.net>
2015-12-09 22:45:57 +0100

    graph achievement status per achievement level
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- hippod/app/assets/js/helper.js
+++ hippod/app/assets/js/helper.js
@@ -115,4 +115,26 @@ function humanFormatDateYYYYMMDDHHMM(date) {
 	return humanRelativeDate(date) + " (" + formatDateYYYYMMDDHHMM(date) + ")";
 }
 
+Date.prototype.getWeek = function (dowOffset) {
 
+    dowOffset = typeof(dowOffset) == 'int' ? dowOffset : 0; //default dowOffset to zero
+    var newYear = new Date(this.getFullYear(),0,1);
+    var day = newYear.getDay() - dowOffset; //the day of week the year begins on
+    day = (day >= 0 ? day : day + 7);
+    var daynum = Math.floor((this.getTime() - newYear.getTime() - 
+    (this.getTimezoneOffset()-newYear.getTimezoneOffset())*60000)/86400000) + 1;
+    var weeknum;
+    if(day < 4) {
+        weeknum = Math.floor((daynum+day-1)/7) + 1;
+        if(weeknum > 52) {
+            nYear = new Date(this.getFullYear() + 1,0,1);
+            nday = nYear.getDay() - dowOffset;
+            nday = nday >= 0 ? nday : nday + 7;
+            weeknum = nday < 4 ? 1 : 53;
+        }
+    }
+    else {
+        weeknum = Math.floor((daynum+day-1)/7);
+    }
+    return weeknum;
+};
--- hippod/app/controllers/item.js
+++ hippod/app/controllers/item.js
@@ -1,15 +1,107 @@
 "use strict";
 
 hippoD.controller("ItemCtrl", function ($scope, $stateParams, $window, DBService, HippodDataService) {
+
 	$scope.id = $stateParams.id
 
-	DBService.getFoo($scope.id).then(function(res) {
-		$scope.data = res;
-	});
+	function updateAchievementChart(res) {
+
+		function getWeekDate(date){
+      var weekNumber = date.getWeek();
+      var year = date.getFullYear();
+			return new Date(year, 0, 1+((weekNumber-1)*7));
+		};
+
+		function getDayDate(date){
+      var day = date.getUTCDate();
+      var month = date.getUTCMonth();
+      var year = date.getUTCFullYear();
+			return new Date(year, month, day, 0, 0, 0);
+		};
 
 
-	HippodDataService.getAchievements($scope.id).then(function(res) {
+    function singularToMult(data) {
+			var o = { };
+      o['pass'] = new Array();
+      o['fail'] = new Array();
+      o['non']  = new Array();
 
+			angular.forEach(data, function(value, key) {
+        var t = new Date(value['date']).getTime();
+        o['pass'].push([t, value['pass']]);
+        o['fail'].push([t, value['fail']]);
+        o['non'].push([t, value['non']]);
+			});
+			return o;
+		};
+
+
+	  var achiev_res_date_arr = new Array();
+
+		angular.forEach(res, function(value, key) {
+			var date = new Date(value['date-added']);
+      var norm_data = getDayDate(date);
+
+			if (achiev_res_date_arr.length <= 0) {
+				var entry = { };
+				entry['date'] = norm_data;
+        entry['pass'] = 0;
+        entry['fail'] = 0;
+        entry['non'] = 0;
+        achiev_res_date_arr.push(entry);
+			} else {
+				if (achiev_res_date_arr[achiev_res_date_arr.length - 1]['date'].getTime() != norm_data.getTime()) {
+					var entry = { };
+					entry['date'] = norm_data;
+					entry['pass'] = 0;
+					entry['fail'] = 0;
+					entry['non'] = 0;
+					achiev_res_date_arr.push(entry);
+				}
+			}
+
+		  var index = achiev_res_date_arr.length - 1;
+
+      console.log(value['result']);
+
+			switch (value['result']) {
+				case "failed":
+      	  achiev_res_date_arr[index]['fail'] += 1;
+					break;
+				case "passed":
+      	  achiev_res_date_arr[index]['pass'] += 1;
+					break;
+				case "nonapplicable":
+      	  achiev_res_date_arr[index]['non'] += 1;
+					break;
+				default:
+					console.log("unknown result: " + value['result']);
+					break;
+			}
+		});
+
+    var mult = singularToMult(achiev_res_date_arr);
+
+		$scope.graphTestResultData = [
+		{
+			"key" : "Passed" ,
+			"values" : mult['pass']
+		},
+
+		{
+			"key" : "Failed" ,
+			"values" : mult['fail']
+		},
+
+		{
+			"key" : "Non Applicable" ,
+			"values" : mult['non']
+		}
+		];
+
+	};
+
+	function updateVaritiesData(res) {
 		var var_data = new Array();
 		angular.forEach(res, function(value, key) {
 			var found_variety = false;
@@ -59,85 +151,59 @@ hippoD.controller("ItemCtrl", function ($scope, $stateParams, $window, DBService
 
 			$scope.varieties = var_data;
 		});
+	};
+
+
+	HippodDataService.getAchievements($scope.id).then(function(res) {
+		updateVaritiesData(res);
+		updateAchievementChart(res);
 	});
 
+	DBService.getFoo($scope.id).then(function(res) {
+		$scope.data = res;
+	});
 
-
-
-
-
-
-
-
-  $scope.graphTestResultOptions = {
-            chart: {
-                type: 'stackedAreaChart',
-                height: 150,
-                margin : {
-                    top: 0,
-                    right: 0,
-                    bottom: 30,
-                    left: 55
-                },
-                x: function(d){return d[0];},
-                y: function(d){return d[1];},
-                useVoronoi: false,
-                clipEdge: true,
-                duration: 100,
-                useInteractiveGuideline: true,
-                xAxis: {
-                    showMaxMin: false,
-                    tickFormat: function(d) {
-                        return d3.time.format('%x')(new Date(d))
-                    }
-                },
-                yAxis: {
-                    tickFormat: function(d){
-                        return d3.format(',.2f')(d);
-                    }
-                },
-								showYAxis: false,
-								showLegend: false,
-								showControls: false,
-                zoom: {
-                    enabled: true,
-                    scaleExtent: [1, 10],
-                    useFixedDomain: false,
-                    useNiceScale: false,
-                    horizontalOff: false,
-                    verticalOff: true,
-                    unzoomEventType: 'dblclick.zoom'
-                }
-            }
-        };
-
-        $scope.graphTestResultData = [
-            {
-                "key" : "Passed" ,
-                "values" : [ [ 1025409600000 , 2] , [ 1028088000000 , 3] ] 
-            },
-
-            {
-                "key" : "Failed" ,
-                "values" : [ [ 1025409600000 , 3] , [ 1028088000000 , 0] ]
-            },
-
-            {
-                "key" : "Non Applicable" ,
-                "values" : [ [ 1025409600000 , 1] , [ 1028088000000 , 0] ]
-            },
-
-            {
-                "key" : "Not Tested" ,
-                "values" : [ [ 1025409600000 , 0] , [ 1028088000000 , 0] ]
-            }
-
-
-
-        ];
-
-
-
+	$scope.graphTestResultOptions = {
+		chart: {
+			type: 'stackedAreaChart',
+			height: 180,
+			margin : {
+				top: 0,
+				right: 50,
+				bottom: 30,
+				left: 5
+			},
+			x: function(d){return d[0];},
+			y: function(d){return d[1];},
+			useVoronoi: false,
+			clipEdge: true,
+			duration: 100,
+			useInteractiveGuideline: true,
+			xAxis: {
+				showMaxMin: false,
+				tickFormat: function(d) {
+					return d3.time.format('%x')(new Date(d))
+				}
+			},
+			yAxis: {
+				tickFormat: function(d){
+					return d3.format(',.0f')(d);
+				}
+			},
+			showYAxis: false,
+			showLegend: false,
+			showControls: false,
+			zoom: {
+				enabled: true,
+				scaleExtent: [1, 10],
+				useFixedDomain: false,
+				useNiceScale: false,
+				horizontalOff: false,
+				verticalOff: true,
+				unzoomEventType: 'dblclick.zoom'
+			}
+		}
+	};
 
 
 });
