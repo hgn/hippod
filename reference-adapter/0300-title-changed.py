076158f31853903c7cccc9e605ae217ada0fe1b6
Alexander Sarmanow <alexsar@gmx.de>
2016-11-30 13:37:06 +0100

    tests for checking new structure
    Signed-off-by: Alexander Sarmanow <alexsar@gmx.de>

---
+++ reference-adapter/0300-title-changed.py
@@ -0,0 +1,238 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+import sys
+import json
+import requests
+import pprint
+import unittest
+import string
+import random
+import os
+import json
+import time
+import datetime
+import base64
+import uuid
+import argparse
+
+pp = pprint.PrettyPrinter(depth=6)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--quite',
+                    help='Just print an OK at the end and fade out the printed data',
+                    action='store_true')
+args = parser.parse_args()
+
+def pprnt(data):
+    if args.quite:
+        pass
+    else:
+        pp.pprint(data)
+
+
+def random_image():
+    with open("data/plot.png", "rb") as f:
+        content = f.read()
+        return base64.b64encode(content)
+
+def random_id():
+    return '0'
+
+
+def random_result():
+    d = ['passed']
+    return d[random.randint(0, len(d) - 1)]
+
+def random_submitter():
+    d = ['Marie Curie']
+    return d[random.randint(0, len(d) - 1)]
+
+def query_full(id):
+    url = 'http://localhost:8080/api/v1/object/{}'.format(id)
+    data = ''' '''
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+    r = requests.get(url, data=data, headers=headers)
+    pprnt("\nStatus Code:")
+    pprnt(r.status_code)
+    pprnt("\nRet Data:")
+    data = r.json()
+    pprnt(data)
+
+def add_n(n):
+    url = 'http://localhost:8080/api/v1/object'
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+
+    for i in range(n):
+        data = dict()
+        data["submitter"] = random_submitter()
+        data["object-item"] = dict()
+        data["object-item"]['categories'] = [ "team:orange", "topic:ip", "subtopic:route-cache" ]
+        data["object-item"]['version'] = '0'
+        data['object-item']['title'] = "Bar"
+
+        data['object-item']['data'] = list()
+        desc_data = dict()
+        desc_data['type'] = 'description'
+        desc_data['mime-type'] = 'text/markdown'
+        # base64 requires a byte array for encoding -> .encode('utf-8')
+        # json requires a string -> convert to UTF-8
+        desc_data['data'] = base64.b64encode(
+                """
+# Rooter: A Methodology for the Typical Unification of Access Points and Redundancy #
+
+Collaboratively administrate empowered **markets** via plug-and-play networks.
+Dynamically procrastinate __B2C users__ after installed base benefits. Dramatically
+visualize customer directed convergence without **revolutionary ROI**.
+
+    int foo(void) {
+        abort(0);
+    }
+
+* Item1
+* Item2
+* Item3
+
+Proctively envisioned multimedia based expertise and cross-media growth
+strategies. Seamlessly visualize quality intellectual capital without superior
+collaboration and idea-sharing. Holistically pontificate installed base portals
+after maintainable products.
+
+![Description](image.png)
+
+
+## Harnessing Byzantine Fault Tolerance Using Classical Theory ##
+
+Efficiently unleash cross-media information without cross-media value. Quickly
+maximize **timely deliverables** for real-time schemas. Dramatically maintain
+clicks-and-mortar __solutions__ without functional solutions.
+
+
+Completely synergize resource taxing relationships via premier niche markets.
+Professionally cultivate one-to-one customer service with robust ideas.
+Dynamically innovate resource-leveling customer service for state of the art
+customer service.
+
+    +---------+
+    |         |                        +--------------+
+    |   NFS   |--+                     |              |
+    |         |  |                 +-->|   CacheFS    |
+    +---------+  |   +----------+  |   |  /dev/hda5   |
+                 |   |          |  |   +--------------+
+    +---------+  +-->|          |  |
+    |         |      |          |--+
+    |   AFS   |----->| FS-Cache |
+    |         |      |          |--+
+    +---------+  +-->|          |  |
+                 |   |          |  |   +--------------+
+    +---------+  |   +----------+  |   |              |
+    |         |  |                 +-->|  CacheFiles  |
+    |  ISOFS  |--+                     |  /var/cache  |
+    |         |                        +--------------+
+    +---------+
+
+Proctively envisioned multimedia based expertise and cross-media growth
+strategies. Seamlessly visualize quality intellectual capital without superior
+collaboration and idea-sharing. Holistically pontificate installed base portals
+after maintainable products.
+
+## Shizzle Dizzle Header Second Order ##
+
+Proctively envisioned multimedia based expertise and cross-media growth
+strategies. Seamlessly visualize quality intellectual capital without superior
+collaboration and idea-sharing. Holistically pontificate installed base portals
+after maintainable products.
+
+                """.encode('utf-8')).decode("utf-8") 
+        data['object-item']['data'].append(desc_data)
+
+        img_data = dict()
+        img_data['name'] = 'image.png'
+        img_data['mime-type'] = 'image/png'
+        img_data['data'] = random_image().decode("utf-8") 
+        data['object-item']['data'].append(img_data)
+
+        img_data = dict()
+        img_data['name'] = 'trace.pcap'
+        img_data['mime-type'] = 'application/vnd.tcpdump.pcap'
+        img_data['data'] = "R0lGODlhDwAPAKECAAAAzxzM/////wAAACwAAAAADwAPAAACIISPeQHsrZ5ModrLlN48CXF8m2iQ3YmmKqVlRtW4MLwWACH+H09wdGltaXplZCBieSBVbGVhZCBTbWFydFNhdmVyIQAAOw=="
+        data['object-item']['data'].append(img_data)
+
+        data["attachment"] = dict()
+        data["attachment"]['references'] = [ "doors:234236", "your-tool:4391843" ]
+        data["attachment"]['tags'] = [ "ip", "route", "cache", "performance" ]
+        data["attachment"]['categories'] = [ "team:orange", "topic:ip", "subtopic:route-cache" ]
+        data["attachment"]['responsible'] = data["submitter"]
+
+        achievement = dict()
+        achievement["test-date"] = datetime.datetime.now().isoformat('T')
+        achievement["result"] = random_result()
+
+        # 1/4 of all achievements are anchored
+        # if random.randint(0, 3) == 0:
+        #     achievement["anchor"] = random_id()
+
+        # add data entry to achievement, can be everything
+        # starting from images, over log files to pcap files, ...
+        achievement['data'] = list()
+        log_data = dict()
+        log_data['name'] = 'result-trace.pcap'
+        log_data['mime-type'] = 'application/vnd.tcpdump.pcap'
+        log_data['data'] = "R0lGODlhDwAPAKECAAABzMzM/////wAAACwAAAAADwAPAAACIISPeQHsrZ5ModrLlN48CXF8m2iQ3YmmKqVlRtW4MLwWACH+H09wdGltaXplZCBieSBVbGVhZCBTbWFydFNhdmVyIQAAOw=="
+        achievement['data'].append(log_data)
+
+        # if random.randint(0, 3) == 0:
+        #     variety = dict()
+        #     variety['os-version'] = 'rhel23'
+        #     variety['platform']   = 'xeon-foo'
+        #     achievement["variety"] = variety
+
+        data["achievements"] = list()
+        data["achievements"].append(achievement)
+
+        #os.system('cls' if os.name == 'nt' else 'clear')
+        pprnt("New Data:\n-----------\n")
+        pprnt(json.dumps(data, sort_keys=True, separators=(',', ': '), indent=4))
+        pprnt("\n-----------\n")
+
+        dj = json.dumps(data, sort_keys=True, separators=(',', ': '))
+        r = requests.post(url, data=dj, headers=headers)
+        pprnt("Return Data:\n-----------\n")
+        ret_data = r.json()
+        pprnt(json.dumps(ret_data, sort_keys=True, separators=(',', ': '), indent=4))
+        assert len(ret_data['data']['id']) > 0
+        processing_time = ret_data['processing-time']
+        sys.stderr.write("\nHTTPStatusCode: {} ServerProcTime {}s\n".format(r.status_code, processing_time))
+
+        query_full(ret_data['data']['id'])
+        time.sleep(1)
+
+    pprnt("\r\n\n")
+    sys.exit(0)
+    pprnt("\r\n\n")
+
+    url = 'http://localhost:8080/api/v1/objects'
+    data = '''
+    {
+        "limit": 0,
+        "ordering": "by-submitting-date-reverse",
+        "maturity-level": "all"
+    }
+    '''
+
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+    r = requests.get(url, data=data, headers=headers)
+    pprnt("\nStatus Code:")
+    pprnt(r.status_code)
+    pprnt("\nRet Data:")
+    data = r.json()
+    pprnt(data)
+    return r.status_code
+
+
+if __name__ == '__main__':
+    status = add_n(1)
+    if status==200:
+        print("OK")
+    else:
+        print("FAIL")
