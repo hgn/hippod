1b73f5f29e2b479fd9f899583d3d6cc18b3a0185
Hagen Paul Pfeifer <hagen@jauu.net>
2017-01-09 21:09:48 +0100

    make script execution path independent
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- tests/0201-check-image-in-achievement.py
+++ tests/0201-check-image-in-achievement.py
@@ -32,7 +32,9 @@ def pprnt(data):
 
 
 def random_image():
-    with open("data/plot.png", "rb") as f:
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    image_path = os.path.join(script_path, "data", "plot.png")
+    with open(image_path, "rb") as f:
         content = f.read()
         return base64.b64encode(content)
 
