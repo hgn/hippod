4282cc911864bd3821bdfc9138eda03cf4f7631b
Alexander Sarmanow <alexsar@gmx.de>
2017-01-16 15:37:04 +0100

    arranged numbers in test names, removed categories
    'Categories' in attachments are removed.
    Fixes #51
    
    Signed-off-by: Alexander Sarmanow <alexsar@gmx.de>

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -3,9 +3,9 @@
 #set -x
 
 TEST=(0101-add-ten-entries.py 0110-add-2000-entries.py)
-TEST2=(check-when-title-changed.sh check-when-categories-changed.sh \
-       check-when-title-and-categories-changed.sh \
-       check-when-one-char-changed.sh check-when-object-twice.sh)
+TEST2=(0604-check-when-title-changed.sh 0600-check-when-categories-changed.sh \
+       0603-check-when-title-and-categories-changed.sh \
+       0602-check-when-one-char-changed.sh 0601-check-when-object-twice.sh)
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
