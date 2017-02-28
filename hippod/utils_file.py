8a1ae3d21b96f1ad2742537a5bf5ce9ac0ce215e
Hagen Paul Pfeifer <hagen.pfeifer@rohde-schwarz.com>
2017-02-28 19:29:18 +0100

    add atomic open/write operation
    Signed-off-by: Hagen Paul Pfeifer <hagen.pfeifer@rohde-schwarz.com>

---
+++ hippod/utils_file.py
@@ -0,0 +1,21 @@
+import os
+import datetime
+
+from contextlib import contextmanager
+
+@contextmanager
+def atomic_write(filepath, binary=False):
+    tmppath = filepath + '~'
+    while os.path.isfile(tmppath):
+        tmppath += '~'
+    try:
+        with open(tmppath, 'wb' if binary else 'w') as file:
+            yield file
+            file.flush()
+            os.fsync(file.fileno())
+        os.rename(tmppath, filepath)
+    finally:
+        try:
+            os.remove(tmppath)
+        except (IOError, OSError):
+            pass
