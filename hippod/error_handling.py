83a8ddd3212bddc67312bffacc12c64974aacb19
muthuubalakan <muthuumuthu@gmail.com>
2018-07-30 14:48:23 +0200

    implement class method

--- hippod/error_handling.py
+++ hippod/error_handling.py
@@ -1,20 +1,28 @@
 from aiohttp import web
-from hippod import hippod_login
 
 
+class ErrorMiddleware:
+    """ErrorMiddleware handles 404 error
 
-ERROR_FILE = open('templates/404.html').read()
-@web.middleware
-async def error_middleware(request, handler):
-    try:
-        response = await handler(request)
-        if response.status != 404:
-            return response
-        message = response.message
-    except web.HTTPException as e:
-        if e.status != 404:
-            raise
-    return web.Response(text=ERROR_FILE, content_type='text/html')
+    Arguments:
+        filename --> Error html file
 
+    Usage:
+        error_middleware = ErrorMiddleware('404.html')
+        app = web.Application(middlewares=[error_middleware.error_middleware])
+    """
 
+    def __init__(self, filename):
+        self._filename = filename
 
+    @web.middleware
+    async def error_middleware(self, request, handler):
+        try:
+            response = await handler(request)
+            if response.status != 404:
+                return response
+        except web.HTTPException as e:
+            if e.status != 404:
+                raise
+        ERROR_FILE = self._filename.read()
+        return web.Response(text=ERROR_FILE, content_type='text/html')
