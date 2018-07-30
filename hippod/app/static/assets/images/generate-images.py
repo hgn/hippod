865af2610ec8c30eefaca23af6af58c43db677f5
muthuubalakan <muthuumuthu@gmail.com>
2018-07-30 14:32:26 +0200

    Put login templates in /hippod/app

---
+++ hippod/app/static/assets/images/generate-images.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import subprocess
+
+def do(cmd):
+    subprocess.call(cmd.split())
+
+
+def generate_and_scale(filename):
+    cmd = "inkscape --export-png={}.png {}.svg".format(filename, filename)
+    do(cmd)
+
+    size = 200
+    cmd  = "convert -filter Triangle -define filter:support=2 "
+    cmd += "-thumbnail {} -unsharp 0.25x0.25+8+0.065 -dither ".format(size)
+    cmd += "None -posterize 136 -quality 82 -define "
+    cmd += "jpeg:fancy-upsampling=off -define png:compression-filter=5 "
+    cmd += "-define png:compression-level=9 -define png:compression-strategy=1 "
+    cmd += "-define png:exclude-chunk=all -interlace none -colorspace sRGB "
+    cmd += "-strip {}.png {}-{}.png".format(filename, filename, size)
+    do(cmd)
+
+    size = 100
+    cmd  = "convert -filter Triangle -define filter:support=2 "
+    cmd += "-thumbnail {} -unsharp 0.25x0.25+8+0.065 -dither ".format(size)
+    cmd += "None -posterize 136 -quality 82 -define "
+    cmd += "jpeg:fancy-upsampling=off -define png:compression-filter=5 "
+    cmd += "-define png:compression-level=9 -define png:compression-strategy=1 "
+    cmd += "-define png:exclude-chunk=all -interlace none -colorspace sRGB "
+    cmd += "-strip {}.png {}-{}.png".format(filename, filename, size)
+    do(cmd)
+
+    size = 40
+    cmd  = "convert -filter Triangle -define filter:support=2 "
+    cmd += "-thumbnail {} -unsharp 0.25x0.25+8+0.065 -dither ".format(size)
+    cmd += "None -posterize 136 -quality 82 -define "
+    cmd += "jpeg:fancy-upsampling=off -define png:compression-filter=5 "
+    cmd += "-define png:compression-level=9 -define png:compression-strategy=1 "
+    cmd += "-define png:exclude-chunk=all -interlace none -colorspace sRGB "
+    cmd += "-strip {}.png {}-{}.png".format(filename, filename, size)
+    do(cmd)
+
+generate_and_scale("hippod-logo")
+generate_and_scale("hippod-logo-ear")
---
+++ hippod/app/static/services/ressources.js
@@ -0,0 +1,69 @@
+"use strict";
+
+hippoD.factory('ResourcesService', function($http) {
+
+    var srv = {};
+
+    srv.getRessource = function() {
+			 var promise = $http(
+				 {
+					 url: '/api/v1.0/resources',
+					 dataType: 'json',
+					 method: 'GET',
+					 data: { },
+					 headers: { "Content-Type": "application/json" }
+			   })
+				 .then(function (response) {
+					 // calculate avg value for mimetype compression
+					 response.data.data['__compression'] = [];
+					 var ptr = response.data.data['data-compression'];
+					 var cnt = 0;
+					 for (var property in ptr) {
+						 var mimetype = property;
+						 var rounds = 0;
+						 var sum = 0;
+						 for (var i = 0; i < ptr[property].length; i++) {
+							 var pct = ptr[property][i]['size-compressed'] / (ptr[property][i]['size-raw'] / 100.0);
+							 sum += pct;
+							 rounds++;
+						 }
+						 if (rounds) {
+							 var avg = sum / rounds;
+							 response.data.data['__compression'][cnt] = new Array(mimetype, avg);
+						 }
+						 cnt++;
+					 }
+
+					 return response;
+				 });
+		 return promise;
+		};
+
+    // Public API
+    return {
+        getRessource: function() {
+            return srv.getRessource();
+        }
+    };
+});
+
+
+hippoD.factory('ResultService', function($http) {
+	return{
+		getResults: function() {
+			var obj = {};
+			var promise = $http({
+	                             url: '/api/v1/cache/achievements',
+	                             dataType: 'json',
+	                             method: 'GET',
+	                             data: obj,
+	                             headers: { "Content-Type": "application/json" }
+                         })
+						 .then(function(response) {
+							return response.data.data;
+						 })
+		return promise
+		}
+	}
+
+});
